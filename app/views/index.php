cba1badecaed9a56dabead4a13da665d6e0a637d
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2021-11-23 10:13:13 +0100

    Adding a structure to our Website

--- app/views/index.php
+++ app/views/index.php
@@ -6,13 +6,43 @@
         require APP_ROOT . '/views/inc/header.php';
     ?>
     <main>
-        <div id="searchbar">
-            <form action="<?= URL_ROOT; ?>/search" method="post">
-                <input type="text" name="search" placeholder="Search..">
-                <button type="submit" name="searchBtn"><i class="fas fa-search"></i></button>
-            </form>
-            
-            
+        <div id="mainPage">
+            <div id="searchbar">
+                <form action="<?= URL_ROOT; ?>/search" method="post">
+                    <input type="text" name="search" placeholder="Search..">
+                    <button type="submit" name="searchBtn"><i class="fas fa-search"></i></button>
+                </form>
+            <div id="symptomList">
+                <h2> <i class="far fa-globe-europe"></i> Popular Symptoms </h2>
+                <ul>
+                    <li class="symptoms">
+                        Cough
+                    </li>
+                    <li class="symptoms">
+                        Fever
+                    </li>
+                    <li class="symptoms">
+                        Stomach Aches
+                    </li>
+                    <li class="symptoms">
+                        Aches and Pains
+                    </li>
+                    <li class="symptoms">
+                        Headache
+                    </li>
+                    <li class="symptoms">
+                        Vomiting
+                    </li>
+                </ul>
+            </div>
+            <div id="infosList">
+                <h2><i class="fad fa-file-medical-alt"></i> Medical News</h2>
+                <div id="info">
+                    <img src="" alt="">
+                    <h3 id="articleTitle"></h3>
+                    <h4 id="name"></h4>
+                </div>
+            </div>
         </div>
     </main>
     <?php
