418ac0d0b6d51792528671b5379bb363d744abfa
Lucas Goncalves <lucasgoncalves@free.fr>
2021-11-26 16:47:39 +0100

    📄 - Update of the disease page

--- app/views/diagnosis/index.php
+++ app/views/diagnosis/index.php
@@ -9,12 +9,18 @@ require APP_ROOT . '/views/inc/head.php';
     <main>
         <div class="diagnosisList class">
             <h2 class="class-title"><i class="fas fa-search"></i> Results</h2>
-            <p><?= $data['issuesResults']['Name'] ?></p>
+            <h2><?= $data['issuesResults']['Name'] ?></h2>
+            <h3>Description : </h3>
             <p><?= $data['issuesResults']['Description'] ?></p>
+            <h3>MedicalCondition : </h3>
             <p><?= $data['issuesResults']['MedicalCondition'] ?></p>
+            <h3>PossibleSymptoms : </h3>
             <p><?= $data['issuesResults']['PossibleSymptoms'] ?></p>
+            <h3>ProfName : </h3>
             <p><?= $data['issuesResults']['ProfName'] ?></p>
+            <h3>Synonyms : </h3>
             <p><?= $data['issuesResults']['Synonyms'] ?></p>
+            <h3>TreatmentDescription : </h3>
             <p><?= $data['issuesResults']['TreatmentDescription'] ?></p>
         </div>
     </main>
