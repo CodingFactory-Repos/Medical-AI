9b6e11a883d76aac5357479df02adafc55a3be38
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-03-28 17:58:56 +0200

    Update header.php

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -4,10 +4,8 @@
         <div class="title">
             <h3>Hey</h3>
             <h2>Medical</h2>
+
         </div>
     </div>
 </header>
 
-<div class="onBack">
-    <img src="<?= URL_ROOT ?>/img/bg.svg">
-</div>
