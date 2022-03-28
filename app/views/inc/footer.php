7553de233694f6989d306d0434dbcb890e72513b
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-03-28 17:58:50 +0200

    Add a 3D animation to our website

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,4 +1,7 @@
 <footer>
+<iframe class="iframe" src='https://my.spline.design/noisedisplacecopy-c9f90177e599b77d0fc586e37cd369cc/' frameborder='0' width='100%' height='100%'></iframe>
+    
+    <div class="hideLogo"></div>
 </footer>
 
 <?php
