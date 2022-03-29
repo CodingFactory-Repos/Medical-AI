4649644a9e46887e7532cbe8b74bbd5d51867847
Lucas Goncalves <lucasgoncalves@free.fr>
2022-03-29 15:24:51 +0200

    Adding some class container

--- app/views/diagnosis/index.php
+++ app/views/diagnosis/index.php
@@ -6,14 +6,21 @@ require APP_ROOT . '/views/inc/head.php';
     <?php
     require APP_ROOT . '/views/inc/header.php';
     ?>
+    <!-- <h3 class="articleTitle visible"><?= substr($data['articles']['articles'][$i]['title'], 0, 27) ?>...</h3> -->
     <main>
         <div class="diagnosisList">
             <h2 class="categoryName"><i class="fas fa-search"></i> Results</h2>
-            <h2 class="diseaseName diseaseText"><?= $data['issuesResults']['Name'] ?></h2>
+            <h2 class="diseaseName diseaseText"><?= $data['issuesResults']['Name']?></h2>
             <h3 class="categoryName">Description : </h3>
-            <p class="diseaseDesc diseaseText"><?= substr($data['issuesResults']['Description'], 0, 80) ?>...</pc>
+            <div class="diseaseContainer">
+                <h4 class="diseaseDesc diseaseText visible"><?= substr($data['issuesResults']['Description'], 0, 80) ?>...</h4>
+                <h4 class="diseaseDesc diseaseText hidden"><?= $data['issuesResults']['Description'] ?></h4>
+            </div>
             <h3 class="categoryName"> Détails : </h3>
-            <p class="diseaseLongDesc diseaseText"> <?= substr($data['issuesResults']['MedicalCondition'], 0, 80) ?>...</p>
+            <div class="longDescContainer">
+                <p class="diseaseLongDesc diseaseText visible"> <?= substr($data['issuesResults']['MedicalCondition'], 0, 80) ?>...</p>
+                <p class="diseaseLongDesc diseaseText hidden"> <?= $data['issuesResults']['MedicalCondition'] ?></p>
+            </div>
             <h3 class="categoryName">Symptômes possibles : </h3>
             <table class="diseaseListSymptoms ">
                 <?php $sympromList = 0;
@@ -47,7 +54,10 @@ require APP_ROOT . '/views/inc/head.php';
                 <?php endforeach; ?>
             </table>
             <h3 class="categoryName">Traitement : </h3>
-            <p class="diseaseTreatment"><?= substr($data['issuesResults']['TreatmentDescription'], 0, 80) ?>...</p>
+            <div class="treatmentContainer">
+                <p class="diseaseTreatment visible"><?= substr($data['issuesResults']['TreatmentDescription'], 0, 80) ?>...</p>
+                <p class="diseaseTreatment hidden"><?= $data['issuesResults']['TreatmentDescription'] ?></p>
+            <div>
         </div>
     </main>
     <?php
