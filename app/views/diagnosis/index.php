12b6f0858cf5fae46bfaaf330fb51576c1a6ce7c
Lucas Goncalves <lucasgoncalves@free.fr>
2022-03-29 10:37:06 +0200

    Link to the script.php

--- app/views/diagnosis/index.php
+++ app/views/diagnosis/index.php
@@ -32,7 +32,7 @@ require APP_ROOT . '/views/inc/head.php';
         </div>
     </main>
     <?php
-    require APP_ROOT . '/views/script.php';
+    require APP_ROOT . '/views/search/script.php';
     ?>
     <?php
     require APP_ROOT . '/views/inc/footer.php';
