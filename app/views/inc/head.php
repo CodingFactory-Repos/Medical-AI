b3f54a770ffc952d32ba2e5ba9a00e076080dd15
Lucas Goncalves <lucasgoncalves@free.fr>
2022-03-30 15:26:25 +0200

    Adding fontawesome

--- app/views/inc/head.php
+++ app/views/inc/head.php
@@ -37,6 +37,11 @@
     <!-- Load FontAwsome and Jquery -->
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
+<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+
+
 </head>
 <body>
 
