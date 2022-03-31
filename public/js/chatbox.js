88865e101cd77ca39c3afb45724fa014cb8a9ce9
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-03-31 14:04:11 +0200

    Update chatbox.js

--- public/js/chatbox.js
+++ public/js/chatbox.js
@@ -78,13 +78,14 @@ $('.chatbox-close').on('click', function(e) {
     $('.fa-robot').fadeIn(300);
 
     $('.fa-robot').css('zIndex', '49');
-    
+
     $('.fa-robot').css('opacity', '1');
     // console.log($('.fa-robot').css('display'));
 
 
 });
 $('.fa-robot').click(function() {
+
     $('.chatbox').fadeIn(300);
     $('.fa-robot').fadeOut(300);
     $('.chatbox').css('zIndex', '50');
